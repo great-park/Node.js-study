33ebcc06d89564e7e149a09b0d98a50a52637039
great-park <cksgh1735@naver.com>
2021-09-19 16:45:06 +0900

    기본 서버

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const http = require('http');
 
-let server = http.createServer();
-server.listen(3000);
+let server = http.createServer(function(request, response){
+  response.end('<h1>Hello world!</h1>');
+});
+server.listen(3001);
