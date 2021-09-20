e691c98ae27c5b3fe40ebd97988af6e2b7723a95
great-park <cksgh1735@naver.com>
2021-09-20 13:06:51 +0900

    routing

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
+users = ['Kim', "Lee", "Park"];
+
 let server = http.createServer(function(request, response){
-  response.end('<h1>Hello world!</h1>');
+  
+  if(request.url === '/'){
+    response.end('<h1>Welcome!</h1>');
+  }else if(request.url === '/users'){
+    response.end('<h1>' + users + '</h1>');
+  }else{
+    response.end('<h1>Page Not Available</h1>');
+  }
 });
 server.listen(3001);
