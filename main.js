fb6769b9eab085453df6a27ec66242919cac552d
great-park <cksgh1735@naver.com>
2021-09-20 13:22:03 +0900

    express 모듈로 라우팅 개선

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const http = require('http');
+const http = require('http'); 
 
-users = ['Kim', "Lee", "Park"];
+const express = require('express');
+const { request, response } = require('express');
 
-let server = http.createServer(function(request, response){
-  
-  if(request.url === '/'){
-    response.end('<h1>Welcome!</h1>');
-  }else if(request.url === '/users'){
-    response.end('<h1>' + users + '</h1>');
-  }else{
-    response.end('<h1>Page Not Available</h1>');
-  }
+const app = express();
+
+app.get('/', (request, response) => {
+  response.end('<h1>Welcome!</h1>');
 });
-server.listen(3001);
+
+app.get('/users', (request, response) => {
+  response.end(`<h1>${users}/h1>`);
+});
+
+app.get('/users/:id', (request, response) => {
+  const userName = users[request.params.id - 1];
+  response.end(`<h1>${userName}/h1>`);
+});
+
+app.get('*', (request, response) => {
+  response.end('<h1>Page Not Available</h1>');
+});
+
+server.listen(3000);
+
