9b95bb05e4722b709f6764f779e0ff0a4eca844a
great-park <cksgh1735@naver.com>
2021-09-21 17:59:59 +0900

    api server

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const http = require('http'); 
+// const express = require('express');
 
-const express = require('express');
-const { request, response } = require('express');
+// const app = express();
 
-const app = express();
+// const users = ['Tom', 'Andy', 'Jessica', 'Paul'];
 
-app.get('/', (request, response) => {
-  response.end('<h1>Welcome!</h1>');
-});
+// app.get('/', (request, response) => {
+//   response.end('<h1>Welcome!</h1>');
+// });
 
-app.get('/users', (request, response) => {
-  response.end(`<h1>${users}/h1>`);
-});
+// app.get('/users', (request, response) => {
+//   response.end(`<h1>${users}</h1>`);
+// });
 
-app.get('/users/:id', (request, response) => {
-  const userName = users[request.params.id - 1];
-  response.end(`<h1>${userName}/h1>`);
-});
+// app.get('/users/:id', (request, response) => {
+//   const userName = users[request.params.id - 1];
+//   response.end(`<h1>${userName}</h1>`);
+// });
 
-app.get('*', (request, response) => {
-  response.end('<h1>Page Not Available</h1>');
-});
+// app.get('*', (request, response) => {
+//   response.end('<h1>Page Not Available</h1>');
+// });
 
-server.listen(3000);
+// app.listen(3000);
 
