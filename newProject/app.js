601da9c4d70d28a353b0f1f8ca3033177aa55e8b
great-park <cksgh1735@naver.com>
2021-11-29 07:19:34 +0900

    미들웨어 추가 및  와일드 카드

--- newProject/app.js
+++ newProject/app.js
@@ -1,15 +1,22 @@
 //nodemon 서버로 실행할 것
 const express = require('express');
+const app = express();
 //경로 처리
 const path = require('path');
 
-
-const app = express();
+/* 모든 요청에서 실행할 코드 작성
+* 미들웨어는 next 매개변수를 실행해줘야 req.params와 일치하는 라우터를 실행
+*/
+app.use((req, res, next)=> {
+    console.log('모든 요청에 실행');
+    next();
+})
 
 //port 라는 속성을 서버에 심는다.
 app.set('port', process.env.PORT || 3000);
 //
 
+//라우터들 
 app.get('/about', (req,res)=>{
     res.send('hello express');
 })
@@ -18,6 +25,12 @@ app.get('/about', (req,res)=>{
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, './index.html'));
 })
+
+//와일드 카드 - 라우팅 분리
+app.get('/category/:name', (req, res)=>{
+    res.send(`hello ${req.params.name}`);
+})
+
 
 
 app.listen(app.get('port'), ()=>{
