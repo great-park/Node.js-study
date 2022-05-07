65da7945aadb57b57c4086b083c04f0c0561303a
great-park <cksgh1735@naver.com>
2022-05-07 17:02:49 +0900

    🔥 clean

---
+++ test.js
@@ -0,0 +1,4 @@
+// 유효성 검사
+const whenTagList = ["A", "B", "H"]; //A : 퇴근 후, B : 출근 전, H : 휴일
+const genderList = ["A", "M", "F"]; //A : 전체, M : 남성, F : 여성
+if (!whenTagList.includes("Q")) console.log(1);
